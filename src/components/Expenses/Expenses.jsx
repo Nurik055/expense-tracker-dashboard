afa5013020d98d34628fd6cb7084dc95648ebc58
Nursultan <nurty1757@gmail.com>
2026-07-04 18:58:49 +0500

    starting changing an expense when bclick

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,11 +1,23 @@
 import "../Expenses/Expenses.css";
+import { useState } from "react";
+
+
+
 function Expenses({ expense, setExpenses }) {
   const thisExpense = expense.id;
+  const [changed, setChanged] = useState("")
   function removeExpense() {
     setExpenses((prev) => prev.filter((e) => e.id !== thisExpense));
   }
+  function changeExpense() {
+    if(changed) {
+      setChanged(false);
+    }else {
+      setChanged(true);
+    }
+  }
   return (
-    <div className="expensesContainer">
+    <div className="expensesContainer" onDoubleClick={changeExpense}>
       <p>{expense.name}</p>
       <p>{expense.amount}</p>
       <p>{expense.description}</p>
